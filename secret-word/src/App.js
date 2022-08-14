f1dd2a515976a5825489d430a9ced9858ec818ba
GHolanda14 <gabriel.raulino14@gmail.com>
2022-08-14 17:45:58 -0300

    consumindo states e inserindo letra

--- secret-word/src/App.js
+++ secret-word/src/App.js
@@ -1,71 +1,86 @@
 //CSS
-import './App.css';
+import "./App.css";
 
 //Data
-import {wordsList} from './data/words';
+import { wordsList } from "./data/words";
 
 //Hooks
-import {useState} from 'react';
+import { useState } from "react";
 
 //Components
-import StartScreen from './components/StartScreen';
-import Game from './components/Game';
-import GameOver from './components/GameOver';
+import StartScreen from "./components/StartScreen";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
 
 const stages = [
-  {id: 1,nome: "start"},
-  {id: 2,nome: "game"},
-  {id: 3,nome: "end"}
-]
+  { id: 1, nome: "start" },
+  { id: 2, nome: "game" },
+  { id: 3, nome: "end" },
+];
 
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].nome);
-  
+
   const [pickedCategory, setPickedCategory] = useState("");
   const [pickedWord, setPickedWord] = useState("");
-  const [leterrs, setLetters] = useState("");
+  const [letters, setLetters] = useState("");
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(3);
+  const [score, setScore] = useState(0);
 
-
+  //Chosing random word and category
   const pickWordAndCategory = () => {
     //Pick random category
     const categories = Object.keys(wordsList);
     const category = categories[Math.floor(Math.random() * categories.length)];
-  
+
     //Pick random word
-    const words = wordsList[category]
+    const words = wordsList[category];
     const word = words[Math.floor(Math.random() * words.length)];
 
-    return {category, word}
-  }
+    return { category, word };
+  };
 
   //Start game
-  const startGame = () =>{
-    const {category,word} = pickWordAndCategory();
+  const startGame = () => {
+    const { category, word } = pickWordAndCategory();
     let wordLetters = word.split("");
 
     wordLetters = wordLetters.map((l) => l.toLowerCase());
-    
-    setPickedCategory(category)
-    setPickedWord(word)
-    setLetters(wordLetters)
+
+    setPickedCategory(category);
+    setPickedWord(word);
+    setLetters(wordLetters);
     setGameStage(stages[1].nome);
-  }
+  };
 
   //Game over
-  const gameOver = () => {
-    setGameStage(stages[2].nome);
-  }  
+  const verifyLetter = (letter) => {
+    console.log(letter);
+  };
 
   //Retry
-  const retry = () =>{
+  const retry = () => {
     setGameStage(stages[0].nome);
-  }
+  };
 
   return (
     <div className="App">
-      {gameStage === stages[0].nome && <StartScreen startGame={startGame}/>}
-      {gameStage === stages[1].nome && <Game gameOver={gameOver} />}
-      {gameStage === stages[2].nome && <GameOver retry={retry}/>}
+      {gameStage === stages[0].nome && <StartScreen startGame={startGame} />}
+      {gameStage === stages[1].nome && (
+        <Game
+          verifyLetter={verifyLetter}
+          pickedCategory={pickedCategory}
+          pickedWord={pickedWord}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          score={score}
+        />
+      )}
+      {gameStage === stages[2].nome && <GameOver retry={retry} />}
     </div>
   );
 }
--- secret-word/src/components/Game.js
+++ secret-word/src/components/Game.js
@@ -1,31 +1,67 @@
-import './Game.css'
+import "./Game.css";
 
-const Game = ({ gameOver }) => {
+import { useState, useRef } from "react";
+
+const Game = ({
+  verifyLetter,
+  pickedCategory,
+  pickedWord,
+  letters,
+  guessedLetters,
+  wrongLetters,
+  guesses,
+  score,
+}) => {
+  const [letter, setLetter] = useState("");
+    const letterInputRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    verifyLetter(letter);
+    setLetter("");
+    letterInputRef.current.focus();
+  };
+
   return (
     <div className="jogo">
       <p className="pontos">
-        Pontuação: <span>000</span>
+        Pontuação: <span>{score}</span>
       </p>
       <h1>Adivinhe a palavra:</h1>
       <h3 className="dica">
-        Dica sobre a palavra: <span>Dica...</span>
+        Dica sobre a palavra: <span>{pickedCategory}</span>
       </h3>
-      <p>VocÊ ainda tem X tentativa(s)</p>
+      <p>Você ainda tem {guesses} tentativa(s)</p>
       <div className="palavraContainer">
-        <span className="letra">A</span>
-        <span className="vazio"></span>
+        {letters.map((l, i) =>
+          guessedLetters.includes(l) ? (
+            <span key={i} className="letra">
+              {l}
+            </span>
+          ) : (
+            <span key={i} className="vazio"></span>
+          )
+        )}
       </div>
       <div className="letrasContainer">
         <p>Tente adivinhar uma letra da palavra:</p>
-        <form>
-          <input type="text" maxLength={1}></input>
-          <button onClick={gameOver}>Jogar</button>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            maxLength="1"
+            onChange={(e) => setLetter(e.target.value)}
+            value={letter}
+            ref={letterInputRef}
+            required
+          />
+          <button type="submit">Jogar</button>
         </form>
       </div>
       <div className="letrasErradasContainer">
         <p>Letras já utilizadas</p>
-        <span>b,</span>
-        <span>a,</span>
+        {wrongLetters.map((l, i) => (
+          <span key={i}>{l}, </span>
+        ))}
       </div>
     </div>
   );
